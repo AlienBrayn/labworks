439f985cddfc1871d61d5f67685e7b57c7a3ae3f
Alex <alienbrayn321@gmail.com>
2019-11-22 22:00:40 +0300

    V5 Лабораторная 8 версия 1

--- stolyarov/Log.php
+++ stolyarov/Log.php
@@ -6,8 +6,13 @@
 	use core;
 	class Log extends core\LogAbstract implements core\LogInterface
 	{
-		public static function log($str){
-            self::Instance()->log[]=$str;
+		public static function log($str)
+        {
+            $file=fopen("Log/log.txt",'a');
+            $write=fwrite($file,date(DATE_COOKIE)." | ".$str."\r\n");
+            fclose($file);
+            if ($write) self::Instance()->log[]=$str;
+            else throw new StolyarovException("Error adding message");
 		}
    		public static function write(){
             self::Instance()->_write();
